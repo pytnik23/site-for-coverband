a8eccfaeb4333bdfbba61c66dfcaef19333a61a6
Oleksii Teplenko <pytnik.tov@gmail.com>
2016-07-22 14:47:41 +0300

    first commit

---
+++ app/mail.php
@@ -0,0 +1,19 @@
+<?php
+if((isset($_POST['date'])&&$_POST['date']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'pytnik.tov@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Дата: '.$_POST['date'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: pwcoverband.com.ua <site@pwcoverband.com.ua>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var gulp 			= require('gulp'),
+	sass 			= require('gulp-sass'),
+	browserSync 	= require('browser-sync'),
+	imagemin 		= require('gulp-imagemin'),
+	del		 		= require('del'),
+	cache			= require('gulp-cache'),
+	autoprefixer	= require('gulp-autoprefixer'),
+	cssmin 			= require('gulp-cssmin'),
+	useref 			= require('gulp-useref'),
+	uglify 			= require('gulp-uglify'),
+	pump 			= require('pump'),
+	wiredep 		= require('wiredep').stream,
+	gulpif 			= require('gulp-if'),
+	runSequence 	= require('run-sequence');
+
+gulp.task('bower', function () {
+	gulp.src('./app/index.html')
+		.pipe(wiredep({
+			directory: 'app/bower_components'
+		}))
+		.pipe(gulp.dest('./app'));
+});
+
+gulp.task('css', function() {
+	return gulp.src('app/scss/**/*.sass')
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		.pipe(autoprefixer({ browsers: ['last 15 versions']}))
+		.pipe(gulp.dest('app/css/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('useref', function () {
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', cssmin()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		}
+	});
+});
+
+gulp.task('imageMin', function() {
+	gulp.src('app/images/**/*')
+		.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}]
+		})))
+		.pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function() {
+	gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('clear', function() {
+	return cache.clearAll();
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist/');
+});
+
+gulp.task('build', function(callback) {
+	runSequence('clean', 'css',
+		['imageMin', 'useref'],
+		callback
+	);
+});
+
+gulp.task('default', ['browser-sync', 'css'], function() {
+	gulp.watch('app/scss/**/*.sass', ['css']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+	gulp.watch('bower.json', ['bower']);
+});
